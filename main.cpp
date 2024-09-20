c2c888a003a3b60fe547f10fb1fca6f6f89ca9c1
HuseyinK0r0glu <korogluh27@gmail.com>
2024-09-20 18:29:21 +0300

    local development FirstProjectnC uploaded to GitHub

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// we use libraries to use features in c++ to add them use include
+#include <iostream>
+
+int main() {
+    // to see the features we have we use std::
+    // cout is short for character out
+    std::cout << "Hello world ";
+    return 0;
+}
